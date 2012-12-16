502b3e44ba9aaa2718055eaae3b2fcd21e8b12cb
unknown <emmanuel.corno@neuf.fr>
2012-12-16 16:35:00 +0100

    Integration des bulles de l'avatar

--- src/spammer/Avatar.java
+++ src/spammer/Avatar.java
@@ -1,9 +1,11 @@
 package spammer;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.state.StateBasedGame;
 
 import backend.GameComponent;
@@ -11,22 +13,63 @@ import backend.geom.Rectangle;
 
 public class Avatar extends GameComponent {
 
+	private enum State{
+		NOTHING, TROLL, CASH, ANGRY
+	}
+	
 	private int x, y;
-	private Image img;
+	private static Image img;
+	private static Image bbTroll;
+	private static Animation bbCash;
+	private static Animation bbAngry;
+	private static State state = State.ANGRY;
+	
+	private static final long BUBBLE_TIME = 1000; //Temps d'affichage d'une bulle
+	private long remainingTime = 0;
 	
 	public Avatar(){
 		x = 0;
 		y = 478;
 		try {
-			img = new Image("assets/enter_avatar.png");
+			if(img == null)
+				img = new Image("assets/enter_avatar.png");
+			if(bbTroll == null)
+				bbTroll = new Image("assets/bad_guy_troll.png");
+			if(bbCash == null){
+				bbCash = new Animation(
+							new SpriteSheet(
+								new Image("assets/bad_guy_combo.png"), 
+								42, 39), 
+							100);
+			}
+			if(bbAngry == null)
+				bbAngry = new Animation(
+							new SpriteSheet(
+								new Image("assets/bad_guy_Angry.png"), 
+								42, 39), 
+							100);
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	@Override
 	public void render(GameContainer gc, StateBasedGame game, Graphics gfx) {
 		img.draw(x, y);
+		switch(state){
+		case NOTHING:
+			break;
+		case TROLL:
+			bbTroll.draw(100, 540);
+			break;
+		case ANGRY:
+			bbAngry.draw(100, 540);
+			break;
+		case CASH:
+			bbCash.draw(100, 540);
+			break;
+		}
 
 	}
 
