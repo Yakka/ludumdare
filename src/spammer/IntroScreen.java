8b03ea2903556d6dad99eff6f673689f04012571
Marc Gilleron <zylann_sagas_mp3@hotmail.fr>
2012-12-16 22:21:05 +0100

    Updated intro screen (needs assets)

--- src/spammer/IntroScreen.java
+++ src/spammer/IntroScreen.java
@@ -2,6 +2,8 @@ package spammer;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -9,44 +11,71 @@ import backend.ui.UIBasicGameState;
 
 public class IntroScreen extends UIBasicGameState
 {
-
+	private int imageIndex;
+	private Image images[];
+	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException
 	{
-		// TODO Auto-generated method stub
-
+		images = new Image[3];
+		// TODO load images
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
+	public void render(GameContainer container, StateBasedGame game, Graphics gfx)
 			throws SlickException
 	{
-		// TODO Auto-generated method stub
-
+		if(imageIndex < images.length)
+		{
+			Image i = images[imageIndex];
+			if(images[imageIndex] != null)
+			{
+				// Draws the image centered
+				gfx.drawImage(i, 
+						(i.getWidth() - container.getWidth()) / 2, 
+						(i.getHeight() - container.getHeight()) / 2);
+			}
+		}
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException
 	{
-		// TODO Auto-generated method stub
-		game.enterState(MainGame.GAME_PLAY);
+		if(imageIndex >= images.length)
+			game.enterState(MainGame.GAME_PLAY);
+		else if(images[imageIndex] == null)
+			imageIndex++;
+	}
+	
+	@Override
+	public void keyPressed(int key, char c)
+	{
+		super.keyPressed(key, c);
+//		if(key == Input.KEY_SPACE || key == Input.KEY_ENTER)
+			imageIndex++;
+	}
+	
+	@Override
+	public void mousePressed(int button, int x, int y)
+	{
+		super.mousePressed(button, x, y);
+		imageIndex++;
 	}
 
 	@Override
 	protected void createUI(GameContainer container, StateBasedGame game)
 			throws SlickException
 	{
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public int getID()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return MainGame.INTRO_SCREEN;
 	}
 
 }
+
+
